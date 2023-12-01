6e45c298f669be84ccf078ae5a02bb5a87efe0de
Buczko <kacper.buczko@capgemini.com>
2023-12-01 14:43:05 +0100

    refactor: city card content

--- apps/angular/projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { CardComponent } from '../../ui/card/card.component';
+import { City } from '../../model/city.model';
+import { CardType } from '../../model/card.model';
+import { FakeHttpService } from '../../data-access/fake-http.service';
+import { CityStore } from '../../data-access/city.store';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `<app-card
+    [list]="cities"
+    [type]="cardType"
+    customClass="bg-light-yellow"
+    ><img src="assets/img/city.jpg" width="200px"
+  /></app-card>`,
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
+  styles: [
+    `
+      ::ng-deep .bg-light-yellow {
+        background-color: rgb(245 229 89);
+      }
+    `,
+  ],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  cities: City[] = [];
+  cardType = CardType.CITY;
+  constructor(private http: FakeHttpService, private store: CityStore) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((c) => this.store.addAll(c));
+
+    this.store.cities$.subscribe((c) => (this.cities = c));
+  }
 }
